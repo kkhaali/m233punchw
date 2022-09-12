9f2d65006bf022b770a6ac11fbc5da3c5512fc79
Pandouby <57749377+Pandouby@users.noreply.github.com>
2022-09-12 13:05:15 +0200

    initial commit

---
+++ src/test/java/com/example/punchclock/PunchClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.punchclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchClockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
