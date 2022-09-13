1f06fa88e2a2dd57e7dd60f05465a866b0f182ce
Pandouby <57749377+Pandouby@users.noreply.github.com>
2022-09-13 09:14:56 +0200

    added validation checkIn and checkOut time

--- src/main/java/com/example/punchclock/controller/EntryController.java
+++ src/main/java/com/example/punchclock/controller/EntryController.java
@@ -3,6 +3,8 @@ package com.example.punchclock.controller;
 import com.example.punchclock.model.Entry;
 import com.example.punchclock.service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,14 +21,24 @@ public class EntryController {
     }
 
     @PostMapping("/entries")
-    public Entry create(@RequestBody Entry entry) {
-        entryService.createEntry(entry);
-        return entry;
+    public ResponseEntity create(@RequestBody Entry entry) {
+        if(entry.getCheckIn().isBefore(entry.getCheckOut())) {
+            entryService.createEntry(entry);
+            return new ResponseEntity(entry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("CheckOut time has to be bevor checkIn time", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/entries/{id}")
-    public Entry update(@RequestBody Entry entry, @PathVariable Long id) {
-        return entryService.update(id, entry);
+    public ResponseEntity update(@RequestBody Entry entry, @PathVariable Long id) {
+        if(entry.getCheckIn().isBefore(entry.getCheckOut())) {
+            entryService.update(id, entry);
+            entry.setId(id);
+            return new ResponseEntity(entry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("CheckOut time has to be bevor checkIn time", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/entries/{id}")
